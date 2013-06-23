533a9739c5920943c0e1e47f04d2596f9e256fd9
Raymond Jacobson <raymondshujacobson8@gmail.com>
2013-06-23 19:39:45 -0400

    add user_id to games

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130623182825) do
+ActiveRecord::Schema.define(:version => 20130623233745) do
 
   create_table "games", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "phrases", :force => true do |t|
